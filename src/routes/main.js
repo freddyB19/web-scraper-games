5f6a7ef0b151a7c0497b617e988d41753c3fd8af
freddyB19 <freddyybolivar2013@gmail.com>
2025-06-02 03:38:57 -0400

    refactor: add cambios al router principal

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,61 +3,67 @@ import { getData } from "@root/utils/getData.js"
 
 
 // Rutas
-import { Index, Header, Footer } from "@base/main.js"
-import { Error404 } from "@errors/main.js"
+import { Error404 } from "@root/BaseSchema/Errors/404.js"
+import { Header } from "@root/BaseSchema/Header/Header.js"
+import { Home } from "@root/BaseSchema/Home/Home.js"
+import { Footer } from "@root/BaseSchema/Footer/Footer.js"
 
 // LOL
-import { LOL } from "@lol/main.js"
-import { urlpatterns } from "@lol/components/nav.js"
-import { LOLChampions } from "@lol/pages/campeones/main.js"
-import { LOLStadistics } from "@lol/pages/estadisticas/main.js"
-import { LOLNews, LOLNotes } from "@lol/pages/noticias_notas/main.js"
+import { LOL } from "@root/LOL/LOL.js"
+import { URLPatterns } from "@root/LOL/components/URLPatterns.js"
+import { LOLChampions } from "@root/LOL/pages/champions/LOLChampions.js"
+import { LOLStadistics } from "@root/LOL/pages/stadistics/LOLStadistics.js"
+import { LOLNews, LOLNotes } from "@root/LOL/pages/news_notes/NewsNotes.js"
 
 
 //Easport
-import { Easport } from "@easport/main.js"
-import { EasportNews } from "@easport/pages/noticias/main.js"
-import { EasportFreeGames } from "@easport/pages/juegos/main.js"
-import { EasportNovelty } from "@easport/pages/novedades/main.js"
-import { EasportUpdates } from "@easport/pages/actualizaciones/main.js"
+import { Easport } from "@root/Easport/Easport.js"
+import { EasportNews } from "@root/Easport/pages/news/EasportNews.js"
+import { EasportFreeGames } from "@root/Easport/pages/games/EasportFreeGames.js"
+import { EasportNovelty } from "@root/Easport/pages/novelty/EasportNovelty.js"
+import { EasportUpdates } from "@root/Easport/pages/updates/EasportUpdates.js"
+
 
 // Noticias
-import { News } from "@noticias/main.js"
-import { Marca } from "@noticias/marca/main.js"
-import { Lanacion } from "@noticias/lanacion/main.js"
-import { DecryptTechnology, DecryptIA } from "@noticias/decrypt/main.js"
-import { ElNacionalTech, ElNacionalIA } from "@noticias/elnacional/main.js"
+import { News } from "@root/Noticias/News.js"
+import { Marca } from "@root/Noticias/pages/marca/Marca.js"
+import { Lanacion } from "@root/Noticias/pages/lanacion/LaNacion.js"
+import { DecryptTechnology } from "@root/Noticias/pages/decrypt/DecryptTech.js"
+import { DecryptIA } from "@root/Noticias/pages/decrypt/DecryptIA.js"
+import { ElNacionalIA } from "@root/Noticias/pages/elnacional/ElNacionalIA.js"
+import { ElNacionalTech } from "@root/Noticias/pages/elnacional/ElNacionalTech.js"
 
+import * as data_json from "@root/result.json"
 
 const routes = {
-  "/": Index,
-  "lol": LOL,
-  "lol_champions": LOLChampions,
-  "lol_estadisticas": LOLStadistics,
-  "lol_noticias": LOLNews,
-  "lol_notas": LOLNotes,
-  "easport": Easport,
-  "easport_noticias": EasportNews,
-  "easport_juegos": EasportFreeGames,
-  "easport_novedades": EasportNovelty,
-  "easport_actualizaciones": EasportUpdates,
-  "noticias": News,
-  "marca": Marca,
-  "lanacion": Lanacion,
-  "decrypt_tecnologia": DecryptTechnology,
-  'decrypt_ia': DecryptIA,
-  'elnacional_tecnologia': ElNacionalTech,
-  'elnacional_ia': ElNacionalIA,
+    "/": Home,
+    "lol": LOL,
+    "lol_champions": LOLChampions,
+    "lol_estadisticas": LOLStadistics,
+    "lol_noticias": LOLNews,
+    "lol_notas": LOLNotes,
+    "easport": Easport,
+    "easport_noticias": EasportNews,
+    "easport_juegos": EasportFreeGames,
+    "easport_novedades": EasportNovelty,
+    "easport_actualizaciones": EasportUpdates,
+    "noticias": News,
+    "marca": Marca,
+    "lanacion": Lanacion,
+    "decrypt_tecnologia": DecryptTechnology,
+    'decrypt_ia': DecryptIA,
+    'elnacional_tecnologia': ElNacionalTech,
+    'elnacional_ia': ElNacionalIA,
 }
 
 export const Routes = async ({}) => {
-  const responsePages = await getData()
-  const route = getHash()
+    /*const responsePages = await getData()*/
+    const responsePages = data_json.default
+    const route = getHash()
 
+    const renderHTML = routes[route] ? routes[route] : Error404
 
-  const renderHTML = routes[route] ? routes[route] : Error404
-  
-  return `
+    return `
     ${await Header({pages: responsePages})}
     <br><br>
     ${await renderHTML({pages: responsePages})}
