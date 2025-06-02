35f08227ff76ae3979aacc40162b91989ab0c9e3
freddyB19 <freddyybolivar2013@gmail.com>
2025-06-02 17:10:38 -0400

    style: corrigiendo espaciado del código en 'getData'

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -2,12 +2,11 @@ const URL = "https://raw.githubusercontent.com/freddyB19/scraper-games/refs/head
 
 
 export const getData = async () => {
-	try{
-		const response = await fetch(URL)
-		const responseJson = await response.json()
-		return responseJson
-	} catch (e) {
-		console.error("Fetch: " + e)
-	}
+    try {
+        const response = await fetch(URL)
+        const responseJson = await response.json()
+        return responseJson
+    } catch (e) {
+        console.error("Fetch: " + e)
+    }
 }
-
