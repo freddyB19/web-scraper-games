1ed96b047ec419872d4cacce5bcfa3a56f000843
freddyB19 <freddyybolivar2013@gmail.com>
2025-06-02 16:59:25 -0400

    fix: corrigiendo objeto para acceder a información en 'ExtraNews'

--- src/BaseSchema/Home/main/ExtraNews.js
+++ src/BaseSchema/Home/main/ExtraNews.js
@@ -1,11 +1,12 @@
 import { Article } from "./components/ArticleNews.js"
 
 export const ExtraNews = ({ notes = [] }) => {
+
     return `
 		${notes.map(note =>
-			(note.principal == undefined)
+			(note.pagina.principal == undefined)
 			? ``
-			: `${Article({note: note.principal, web: note.web})}`
+			: `${Article({note: note.pagina.principal, web: note.nombre})}`
 		).join('')}
 	`
 }
